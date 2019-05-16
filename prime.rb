4764a64ca600b3ba84a2f5466dd0b9d35e321bce
Max Sun <max.sun@yale.edu>
2019-05-16 01:01:28 +0000

    Primes

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  elsif number == 2
+    return true
+  else
+    test = 2
+    loop do
+      if number % test == 0
+        return false
+        break
+      elsif test > Math.sqrt(number)
+        return true
+        break
+      else
+        test += 1
+      end
+    end
+  end
+end
