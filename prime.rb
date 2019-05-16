3fbd70fb14c623de85b0a82a4e11354c1ea84366
Max Sun <max.sun@yale.edu>
2019-05-16 00:21:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,16 +2,17 @@
 def prime?(number)
   test = 2
   return true
-  while test <= Math.sqrt(number)
-    if number % test == 0
-      return false
-      break
-    elsif number < 2
-      return false
-      break
-    else
-      test += 1
-      return true
+  if number < 2
+    return false
+  else
+    while test <= Math.sqrt(number)
+      if number % test == 0
+        return false
+        break
+      else
+        test += 1
+        return true
+      end
     end
   end
 end
