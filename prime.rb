d66d9f4671d9a459f76ca55d3d218270ce4e6b9c
Max Sun <max.sun@yale.edu>
2019-05-16 00:31:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,9 @@
 # Add  code here!
 def prime?(number)
   test = 2
-  return false
   if number < test
     return false
   else
-    while test < number
-      if number % test == 0
-        return false
-        break
-      else
-        test += 1
-        return true
-      end
-    end
+
   end
 end
